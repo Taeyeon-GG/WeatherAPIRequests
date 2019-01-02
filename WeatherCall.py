1e7ed4e5ea0b1b326cdb8770216165cb6d118f39
Ryan Leigh <ccearle@ad.ucl.ac.uk>
2019-01-02 11:25:39 +0000

    Complient with PEP-8

--- WeatherCall.py
+++ WeatherCall.py
@@ -1,43 +1,59 @@
 import requests # HTTP Requests.
 import re       # Regular Expressions.
 
-# Get API Key from Open Weather Maps
-APIKEY = ""
+# Get API Key from Open Weather Maps.
+APIKEY = "ac5940b013891b1e87b89938adf18cf8"
 
 
-def getLocation(): # Prompt user for location.
+def get_location():
     while(True):
+        location = input("Location: ")
 
-        location = input("Location: ") # Get user input for location & remove whitespace
-
-        if (location == ""): # Check if location string is empty, if so ask again.
+        # Check if location string is empty, if so ask again.
+        if (location == ""):
             print("Please enter a location")
             continue
-        if (re.search(r'[^a-zA-Z,]', location)): # Check if location string contains anything other than letters, if so ask again.
+
+        # Check if location string contains anything other than letters, if so ask again.
+        if (re.search(r'[^a-zA-Z, ]', location)):
             print("Please enter a location without numbers or symbols")
             continue
-        else:
-            location.replace(" ", "") # remove whitespace from location string.
-            return location
+
+        # remove whitespace from location string.
+        location.replace(" ", "")
+
+        return location
 
 
-def sendRequest(APIKEY): # Supply APIKEY as parameter
-    return requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + getLocation() + "&APPID=" + APIKEY) # Send API call with location parameter and API Key.
+def send_request(APIKEY):
+    # Send API call with location parameter and API Key.
+    return requests.get("http://api.openweathermap.org/data/2.5/weather?q="
+                        + get_location()
+                        + "&APPID="
+                        + APIKEY)
 
 
-def getResults(json_results):
-
+def get_results(json_results):
     try:
+        # Get name of returned data.
         loc_name = json_results["name"]
+
+        # Get Country Code of returned data.
         loc_id = json_results["sys"]["country"]
-        desc = re.search("description': '(.+?)',",str(json_results["weather"][0])).group(1) # User Regular Expression to get description of location.
-        temp_celsius = round(float(json_results["main"]["temp"]) - 273.15, 1) # Convert returned temp (Kelvin) to Celsius.
 
-        print(f"The Temperature in {loc_name}, {loc_id} is {temp_celsius}℃. The description given is: '{desc}'")
+        # User Regular Expression to get description of returned data.
+        desc = re.search("description': '(.+?)',",str(json_results["weather"][0])).group(1)
 
-    except KeyError: # make sure valid data was returned from the API call, if not, start again.
+        # Convert returned temp (Kelvin) to Celsius.
+        temp_celsius = round(float(json_results["main"]["temp"]) - 273.15, 1)
+
+    except KeyError:  # Handle error when API returns unusable or invalid data.
         print("Was not able to find information on this location, did you spell it right?\n")
-        getResults(sendRequest(APIKEY).json())
+
+        # Start process again.
+        get_results(send_request(APIKEY).json())
+
+    return print(f"The Temperature in {loc_name}, {loc_id} is {temp_celsius}℃. The description given is: '{desc}'")
 
 
-getResults(sendRequest(APIKEY).json()) # Start process on file run.
+get_results(send_request(APIKEY).json())  # Start process on initialisation.
